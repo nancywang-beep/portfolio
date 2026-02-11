185eb410d6e4d1026f02d0c7bcddcc1e93b47d80
nancywang-beep <wyf4application@gmail.com>
2026-02-10 19:07:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Simple Console Greeting (Engineering Easter Egg)
+console.log("Portfolio loaded. Welcome, fellow engineer.");
